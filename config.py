d45d577fb2a276df50fcece262b5e158dd2b2d3f
zhen_hai <113818064+zhenHai1021@users.noreply.github.com>
2023-09-07 23:26:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c97pmphwoykd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "huezhenwei-employee"
+customregion = "us-east-1"
+
